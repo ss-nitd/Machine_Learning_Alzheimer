eb8955a56e161c12ad34e286b5e56a581178f064
ss-nitd <soumi.sarkar48@gmail.com>
2017-07-07 18:55:18 -0400

    Add files via upload

---
+++ src/code/parser.java
@@ -0,0 +1,17 @@
+package code;
+
+import edu.stanford.nlp.process.CoreLabelTokenFactory; 
+import edu.stanford.nlp.process.DocumentPreprocessor; 
+import edu.stanford.nlp.process.PTBTokenizer; 
+import edu.stanford.nlp.ling.CoreLabel;   
+import edu.stanford.nlp.ling.HasWord;   
+import edu.stanford.nlp.trees.*; 
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser; 
+
+class parser 
+{
+	void Stanford_parser()
+	{
+	
+	}
+}
